d58d5e5e69ddd9a1eb5cf3ab0c7970d2ace9d34c
bbangg <bbanggdev@gmail.com>
2023-02-04 10:02:53 +0300

    first commit

---
+++ example/app/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <tray.hpp>
+
+int main()
+{
+    Tray::Tray tray("Test App", "icon.ico");
+
+    tray.addEntry(Tray::Button("Test"))->setDisabled(true);
+    tray.addEntry(Tray::Separator());
+    tray.addEntry(Tray::Label("Test Label"));
+    tray.addEntry(Tray::Toggle("Test Toggle", false, [](bool state) { printf("State: %i\n", state); }));
+
+    tray.addEntry(Tray::Separator());
+    tray.addEntry(Tray::Submenu("Test Submenu"))->addEntry(Tray::Button("Submenu button!"))->setDisabled(true);
+    tray.addEntry(Tray::Separator());
+    tray.addEntry(Tray::Button("Exit", [&] { tray.exit(); }));
+
+    tray.run();
+
+    return 0;
+}
